a67fe541ebda07f936b1a53e6ac70b05e0ab0b3c
StanislavKosy <Stanislav.kosyy@yandex.ru>
2022-11-03 01:34:08 +0300

    настроил скрипт файл + джикверю + модальное окно есть

---
+++ public/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+   $("#button_for_itineraries").click(function() {
+   		$('#box_for_itineraries').empty();
+   		$('#box_for_itineraries').append('<div>маршрут1</div>');
+		//alert("Handler for .click() called.");
+   });
+});
+
+
+
+ 
+  
---
+++ src/routes/itineraries.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/itineraries', function (req, res) {
+  //res.render("admin/main.hbs")
+})
+
+module.exports = router;
