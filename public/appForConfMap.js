0be4daac15f7cedf9ced7fe6000c26e4fd4e5170
StanislavKosy <StanislavKosy@yandex.ru>
2022-11-14 15:59:36 +0300

    update

--- public/appForConfMap.js
+++ public/appForConfMap.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
-	$("#box_for_picture_map").append("<img src = \"/images/realMap.jpeg\">")
-	/*$("#box_for_picture_map").mousedown() {
-		alert("нажал")
-	}*/
+	$("#box_for_picture_map").append("<img id=\"current_map\" src = \"/images/realMap.jpeg\">")
+	
+
+//это вертим div
+	var rotated = false;
+
+    var div = document.getElementById('my_div');
+    var deg = rotated ? 0 : -45;
+
+    div.style.webkitTransform = 'rotate('+deg+'deg)'; 
+    div.style.mozTransform    = 'rotate('+deg+'deg)'; 
+    div.style.msTransform     = 'rotate('+deg+'deg)'; 
+    div.style.oTransform      = 'rotate('+deg+'deg)'; 
+    div.style.transform       = 'rotate('+deg+'deg)'; 
+
+    rotated = !rotated;
 })
